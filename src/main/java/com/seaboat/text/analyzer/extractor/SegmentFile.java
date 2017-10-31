278f7575a3a4a0cb961c1ad680e026d6c0302c37
wangjian <849586227@qq.com>
2017-10-31 16:36:24 +0800

    preprocess samples

---
+++ src/main/java/com/seaboat/text/analyzer/extractor/SegmentFile.java
@@ -0,0 +1,41 @@
+package com.seaboat.text.analyzer.extractor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.seaboat.text.analyzer.util.DataReader;
+import com.seaboat.text.analyzer.util.DataWriter;
+import com.seaboat.text.analyzer.util.WordSegmentUtil;
+
+/**
+ * 
+ * @author seaboat
+ * @date 2017-10-31
+ * @version 1.0
+ * <pre><b>email: </b>849586227@qq.com</pre>
+ * <pre><b>blog: </b>http://blog.csdn.net/wangyangzhizhou</pre>
+ * <p>preprocess the samples. for we want to tag the named entity, 
+ * so we should do word segment first,and mark the position of the named entity.</p>
+ */
+public class SegmentFile {
+
+  private static String FILE = "data/test.txt";
+  private static String segFILE = "data/tag_test.txt";
+
+  public static void main(String[] args) {
+    List<String> texts = DataReader.readContent(FILE);
+    List<String> segTexts = new ArrayList<String>();
+    for (String text : texts) {
+      List<String> words = WordSegmentUtil.seg(text);
+      String segmented = "";
+      for (String word : words) {
+        segmented += word + "/";
+      }
+      segmented = segmented.substring(0, segmented.length() - 1);
+      segmented = "###"+segmented+"### ###";
+      segTexts.add(segmented);
+    }
+    DataWriter.writeContent(segFILE, segTexts);
+  }
+
+}
---
+++ src/main/java/com/seaboat/text/analyzer/util/DataWriter.java
@@ -0,0 +1,50 @@
+package com.seaboat.text.analyzer.util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author seaboat
+ * @date 2017-09-19
+ * @version 1.0
+ * <pre><b>email: </b>849586227@qq.com</pre>
+ * <pre><b>blog: </b>http://blog.csdn.net/wangyangzhizhou</pre>
+ * <p>Data Writer.</p>
+ */
+public class DataWriter {
+
+  public static void writeContent(String f, List<String> contents) {
+    File file = new File(f);
+    FileWriter fw = null;
+    BufferedWriter writer = null;
+    try {
+      fw = new FileWriter(file);
+      writer = new BufferedWriter(fw);
+      for (String s : contents) {
+        writer.write(s);
+        writer.newLine();
+      }
+      writer.flush();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        writer.close();
+        fw.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+}
